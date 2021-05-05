ba9b4cbdb74fee7ce0abaf4e4835eb360f1bffd7
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2021-05-05 12:18:27 +0300

    Update SPACE.js

--- JS/SPACE.js
+++ JS/SPACE.js
@@ -13,11 +13,11 @@ const shipi2  = new Image();
 shipi2.src = "icon/SpaceI2.png"
 const death = 0
 
-let dfell="icon/deathif.png";
-let gfell="icon/groundsf.png";
-let tfell="icon/tankf.png";
-let ffell="icon/SpaceI1f.png";
-let sfell="icon/SpaceIf.png";
+let dfell="icon/deathiF.png";
+let gfell="icon/groundsF.png";
+let tfell="icon/tankF.png";
+let ffell="icon/SpaceI1F.png";
+let sfell="icon/SpaceIF.png";
 
 
 let livet = [0, 1, 2];
